7a3ec171c58e895cae2803be7ecc8be296046bda
dennisneuendorfnetlight <dennis.neuendorf@netlight.com>
2017-01-29 11:56:49 +0100

    add comments

--- backend/websocket.ts
+++ backend/websocket.ts
@@ -1,34 +1,39 @@
 import * as socketIo from 'socket.io';
 import * as _ from 'lodash';
 
-//const sessions = [];
 const users = {};
 
 export const initializeWebSocket = (server) => {
     const socketServer = socketIo.listen(server);
     socketServer.on('connection', socket => {
-        // socket answer, when connection is established
-        //socket.emit('connect', { time: new Date(), text: 'Connected to Server!' });
-        //sessions.push(socket);
+        //send back sessionId
         socket.emit('session',{sessionId: socket.id});
-        // socket broadcast
-        socket.on('chat', (data) => {
-            console.log('chat', data);
-            let session = data.recipient && users[data.recipient] && socketServer.sockets.connected[users[data.recipient].sessionId];
-            console.log('sessionID',users[data.recipient].sessionId);
-            if(session){
-                session.emit('chat', data);
-            }
-            else{
-                console.log("No session found");
-            }       
-        });
 
+        //register username with sessionid in users and publish list to all sessions
         socket.on('register', (data) => {
             if(data && data.username){
                 users[data.username] = data;
                 socket.emit('clients', _.values(users));
             }
         });
+
+        //send chat-message to recipient
+        socket.on('chat', (data) => {
+            //resolve recipients-session
+            let session = data.recipient && users[data.recipient] && socketServer.sockets.connected[users[data.recipient].sessionId];
+            if(session){
+                session.emit('chat', data);
+            }
+            else{
+                if(data.recipient){
+                    console.log("No session found for " + data.recipient);
+                }
+                else{
+                    console.log("No recipient");
+                }
+            }       
+        });
+
+        
     });
 }
