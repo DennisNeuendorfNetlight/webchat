5622094398e04e37a181b1d0fb8d241a497f6068
dennisneuendorfnetlight <dennis.neuendorf@netlight.com>
2017-01-29 03:11:02 +0100

    add sessionmanagement

--- backend/websocket.ts
+++ backend/websocket.ts
@@ -1,14 +1,34 @@
 import * as socketIo from 'socket.io';
+import * as _ from 'lodash';
+
+const sessions = [];
+const users = {};
 
 export const initializeWebSocket = (server) => {
     const socketServer = socketIo.listen(server);
     socketServer.on('connection', socket => {
         // socket answer, when connection is established
-        socket.emit('connect', { time: new Date(), text: 'Connected to Server!' });
+        //socket.emit('connect', { time: new Date(), text: 'Connected to Server!' });
+        sessions.push(socket);
+        socket.emit('session',{sessionId: socket.id});
         // socket broadcast
         socket.on('chat', (data) => {
             console.log('chat', data);
-            socketServer.emit('chat', data);
+            let session = data.recipient && users[data.recipient] && sessions[users[data.recipient]];
+            if(session){
+                session.emit('chat', data);
+            }
+            else{
+                console.log("No session found");
+            }       
+        });
+
+        socket.on('register', (data) => {
+            console.log('register', data);
+            if(data && data.username){
+                users[data.username] = data;
+                socket.emit('clients', _.values(users));
+            }
         });
     });
 }
